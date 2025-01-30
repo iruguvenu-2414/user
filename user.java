64b74d08419882b709f838226c7e999ddfdae5eb
iruguvenu-2414 <iruguvenu003@gmail.com>
2025-01-30 12:16:35 +0530

    Add files via upload

---
+++ user.java
@@ -0,0 +1,17 @@
+// Source code is decompiled from a .class file using FernFlower decompiler.
+class User {
+    String username;
+    String password;
+    String email;
+    Train bookedTrain;
+    int seatNumber;
+ 
+    public User(String var1, String var2, String var3) {
+       this.username = var1;
+       this.password = var2;
+       this.email = var3;
+       this.bookedTrain = null;
+       this.seatNumber = -1;
+    }
+ }
+ 
